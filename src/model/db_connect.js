d4a1d0cdb803bcc25f072253ec6424353b0b9910
IsraaMoqbel <isramm94@gmail.com>
2018-05-12 14:28:32 +0300

    heroku

--- src/model/db_connect.js
+++ src/model/db_connect.js
@@ -1,4 +1,4 @@
 const {Pool}= require("pg");
 require("env2")("./config.env");
-if(!process.env.HEROKU_POSTGRESQL_IVORY_URL) throw new Error("DB_URL not set");
+if(!process.env.DATABASE_URL) throw new Error("DB_URL not set");
 module.exports= new Pool({connectionString: process.env.DATABASE_URL, ssl:true })
